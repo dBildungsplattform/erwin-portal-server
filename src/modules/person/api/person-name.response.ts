1c871a29ede294766d030806f232878daab1fb42
Fshmit <122355627+Fshmit@users.noreply.github.com>
2025-11-04 08:39:35 +0100

    EW-1327 changed the names of rollenart and familiennamen (#29)

--- src/modules/person/api/person-name.response.ts
+++ src/modules/person/api/person-name.response.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class PersonNameResponse {
     @ApiProperty()
-    public readonly familiennamen: string;
+    public readonly familienname: string;
 
     @ApiProperty()
     public readonly vorname: string;
@@ -32,7 +32,7 @@ export class PersonNameResponse {
     public readonly sortierindex?: string;
 
     public constructor(props: Readonly<PersonNameResponse>) {
-        this.familiennamen = props.familiennamen;
+        this.familienname = props.familienname;
         this.vorname = props.vorname;
         this.initialenfamilienname = props.initialenfamilienname;
         this.initialenvorname = props.initialenvorname;
