793cb9c0d9767b535ecebf8c38678de28cc8cdf0
Hussam Kayed <hussam.kayed@capgemini.com>
2026-01-22 10:33:26 +0100

    EW-1348: added api properties to enable swagger generation to the return object of the endpoint

--- src/modules/rolle/api/rolle-name-id.response.ts
+++ src/modules/rolle/api/rolle-name-id.response.ts
@@ -1,8 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Rolle } from '../domain/rolle.js';
 
 export class RolleNameIdResponse {
+    @ApiProperty()
     public id: string;
 
+    @ApiProperty()
     public name: string;
 
     public constructor(rolle: Rolle<true>) {
